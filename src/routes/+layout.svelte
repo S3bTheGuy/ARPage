b3f01bfa93fa6e1ac167f5dc2974cfc50bde37b2
SomeGuy <51111827+S3bTheGuy@users.noreply.github.com>
2023-08-31 14:24:49 +0200

    end me

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,7 +25,7 @@
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		Lavet af Mikkel M. H. Pedersen & Sebastian F. Steffensen | Ved brug af SvelteKit
 	</footer>
 </div>
 
